3aad5350875d719f4e8fc04ff1570d5510a78789
Geetansh <geetansh323.be22@chitkara.edu.in>
2024-04-28 14:37:09 +0530

    Deploy check

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-center">
-        <Link to="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <Link to="/general" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <img src={news} alt="News" className="w-14 h-14 rounded-full" />
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center md:justify-end font-serif">
